5ffaa9aad89f9a15ca8b99f854c89b8143ea4420
Marcel Campos <marcel_campos@estudante.sesisenai.org.br>
2023-02-17 16:56:33 -0300

    Conexão realizada com o banco de dados

--- labbeach/src/main/java/br/com/labparadises/labbeach/models/Bairro.java
+++ labbeach/src/main/java/br/com/labparadises/labbeach/models/Bairro.java
@@ -1,4 +1,17 @@
 package br.com.labparadises.labbeach.models;
 
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Setter @Getter @NoArgsConstructor @AllArgsConstructor @ToString @EqualsAndHashCode
+
+
 public class Bairro {
+
+    //Colunas no do banco de dados
+    private Long id;    // Id Auto incrementável - JPA gera automáticamente
+    private String nome;
+    private String descricao;
+    private Integer populacao;
+
 }
