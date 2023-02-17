95ca53a587aa04170e14bff96bf87160a3cae21c
Marcel Campos <marcel_campos@estudante.sesisenai.org.br>
2023-02-17 13:42:27 -0300

    Inicialização do projeto

---
+++ labbeach/src/test/java/br/com/labparadises/labbeach/LabbeachApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.labparadises.labbeach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabbeachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
