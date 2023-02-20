bd6516c6bb748a1187ed559eb18eefa00a925705
Marcel Campos <marcel_campos@estudante.sesisenai.org.br>
2023-02-20 17:14:24 -0300

    Bairro: Endpoint, Dependências services/controllers

--- labbeach/src/main/java/br/com/labparadises/labbeach/controllers/BairroControllers.java
+++ labbeach/src/main/java/br/com/labparadises/labbeach/controllers/BairroControllers.java
@@ -1,4 +1,63 @@
 package br.com.labparadises.labbeach.controllers;
 
+//Classe BairroControllers -> organiza o endpoint: o endereço onde a API será acessada.
+
+import br.com.labparadises.labbeach.models.Bairro;
+import br.com.labparadises.labbeach.services.BairroServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController //Anotação de classe
+@RequestMapping("/bairros") //Endpoint -> caminho mapeado com String.
+
 public class BairroControllers {
+
+    @Autowired
+    BairroServices services; //*Injetar Dependência do services.
+
+    //*Criar Endpoint para chamar o método salvar
+    //*Com o Endpoint criado, ele vai receber os dados do bairro para salvar no banco de dados, sendo um Endpoint do tipo post.
+    //*Dentro do método cadastrar, vamos chamar o método do service. Para isso, precisamos injetar a dependência do services, utilizando o @Autowired.
+
+    //*Dentro do cadastro pode fazer service.salvar(bairro)
+    /*
+     Anotação PostMapping,não precisa colocar o caminho porque sempre que receber um Request do tipo Post neste caminho
+     bairros, o Spring evoca este método cadastrado.
+     */
+    @PostMapping
+    /*
+    Difinir de que forma o parâmetro bairro vai chegar pra gente, onde exatamente vai estar na requisição http://
+    Para isso adicionamos uma anotação @RequestBody, esse valor vai vir no corpo da requisição http://
+
+     */
+    public void cadastrar(@RequestBody Bairro bairro) {
+        services.salvar(bairro);
+
+
+    }
+
+
 }
+
+
+    /*
+    OBS:.Temos um método que não é chamado para lugar nenhum, temos que transformar em um endpoint, mapear o
+    BairroControllers para sempre que a nossa API receber uma requisição no caminho:-> /bairros -> cair dentro de
+    BairroControllers, para isso adiciona a anotação @RequestMapping e coloca uma String do caminho que queremos mapear.
+     */
+
+    /*
+    OBS:.Para cadastrar um novo bairro na nossa API, precisamos enviar uma nova requisição http do tipo post para a URL:
+    http://localhost:8080/bairros, lembrando que localhost é porque a API está sendo rodada localmente na nossa máquina.
+    Se fosse em outro servidor seria o domínio para chegar em outro servidor.
+    */
+
+    /*
+    OBS:.Como fazer uma requisição  http se não há um Front-End, não há um lado cliente para comunicar com este lado
+    servidor, com a nossa API. Por esse motivo usamos a ferramenta chamada Postman ou o Insomnia utilizada para testar a
+    API, esses nos serviços WEB sem a necessidade de estar rodando um Front-End da Aplicação.
+    */
--- labbeach/src/main/java/br/com/labparadises/labbeach/services/BairroServices.java
+++ labbeach/src/main/java/br/com/labparadises/labbeach/services/BairroServices.java
@@ -1,32 +1,46 @@
 package br.com.labparadises.labbeach.services;
 
+import br.com.labparadises.labbeach.models.Bairro;
 import br.com.labparadises.labbeach.repositories.BairroRepository;
 import org.springframework.stereotype.Service;
 
-//Na camada  de Service importamos um Repository da entidade (Bairro) e invocamos um método (função, subrotina) apartir de uma instância (novo objetodo criado no mesmo tipo da classe) desse repository
+///Classe BairroServices -> comportamentos que não se encaixam em outras classes de forma natural e encapsulada.
+// Service: Realizado as operações de inserção, deleção, atualização, etc.
+
+//Na camada  de Service importamos um Repository da entidade (Bairro) e evocamos um método (função, subrotina) apartir de uma instância (novo objetodo criado no mesmo tipo da classe) desse repository
 //Como fazemos uma instância do (ex: BairroRepository) se o BairroRepository é uma interface.
 //Aqui entramos no conceito de injeçao de depenência, que o um designer father ou um padrão de projeto que define que uma classe não pode instanciar um objeto que ela mesmo precisaria instaciar.
 //Esse Objeto que ela precisa utilizar, precisa ser fornecido pronto, já instanciado para a classe que irá utilizar ele.
 //Spring facilita justamente essa injeção de indepêndencia.
 
-@Service //Anotação de classe
+
+//Classe BairroServices: importar requisito da entidade e invoca método que........
+
+@Service //*Anotação de classe
+
 public class BairroServices {
 
     //Dentro da nossa classe BairroServices criamos um atributo do tipo Repository (BairroRepository)
     //Desta forma podemos receber uma instancia de Bairro
 
 
-    private BairroRepository repository; //Atributo do tipo Repository
+    private BairroRepository repository; //*Atributo do tipo Repository
 
-    //Receber uma instância de BairroRepository no construtor dessa classe BairroServices e atribuir ao noso atributo
+    //Receber uma instância de BairroRepository no construtor dessa classe BairroServices e atribuir o nosso atributo
     //Assim podemos utilizar (BairroRepository) nos métodos da classe na sequência
 
     //Definição de construtor (método cujo nome deve ser o mesmo nome da classe e sem indicação do tipo de retorno) de BairroService.
 
-    //Construtor recebendo (BairroRepository) e vai atribuir ao this.repository o que le recebeu por parâmetro (repository)
 
+
+    //*Definimos o construtor: BairroServices recebe BairroRepository e atribui ao this.repository que recebe por parâmetro o repository. Assim injetando a classe BairroRepository dependência a classe BairroServices.
     public BairroServices(BairroRepository repository) {
         this.repository = repository;
-}
+    }
+
+    //*Criamos o Método (intrução) Salvar (no banco de dados), que recebe o objeto Bairro e invoca o save do BairroRepository
+    public void salvar(Bairro bairro){
+        repository.save(bairro);
+    }
 
 }
